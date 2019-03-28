f63b7d394e0b941f7515679d66c53014f012fd9a
Boris Nekezov <boris.nekezov@gmail.com>
2019-03-28 16:57:50 +0200

    Update application

--- 11-vue-router/src/components/pages/PageAbout.vue
+++ 11-vue-router/src/components/pages/PageAbout.vue
@@ -4,6 +4,9 @@
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic atque reprehenderit tempore, alias officiis facere, nam animi porro est impedit consequatur adipisci modi harum fugit provident molestiae deserunt illo cupiditate. Officiis tempore sint reiciendis numquam molestiae quae laudantium quis laborum in eaque omnis quam sapiente consequuntur voluptatem eligendi odio beatae nihil quo exercitationem, nisi dicta nostrum maiores deleniti? Officiis beatae, in voluptate esse ratione voluptates deleniti ipsum ullam odit incidunt molestias quod quibusdam quasi dolores vel, laborum reiciendis assumenda velit consequatur possimus, dolorum nobis tempora! Aut doloremque, vero a placeat odit blanditiis iste nesciunt, dicta labore totam repellat quas mollitia.
         </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic atque reprehenderit tempore, alias officiis facere, nam animi porro est impedit consequatur adipisci modi harum fugit provident molestiae deserunt illo cupiditate. Officiis tempore sint reiciendis numquam molestiae quae laudantium quis laborum in eaque omnis quam sapiente consequuntur voluptatem eligendi odio beatae nihil quo exercitationem, nisi dicta nostrum maiores deleniti? Officiis beatae, in voluptate esse ratione voluptates deleniti ipsum ullam odit incidunt molestias quod quibusdam quasi dolores vel, laborum reiciendis assumenda velit consequatur possimus, dolorum nobis tempora! Aut doloremque, vero a placeat odit blanditiis iste nesciunt, dicta labore totam repellat quas mollitia.
+        </p>
     </div>    
 </template>
 
